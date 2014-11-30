fc52ac192981c90022e107e39f9ff4f39cac70af
Sandro Mancuso <sandromancuso@yahoo.com>
2014-11-30 20:50:27 +0100

    letter parameter renamed to stopLetter

--- src/main/java/com/codurance/Diamond.java
+++ src/main/java/com/codurance/Diamond.java
@@ -6,8 +6,8 @@ public class Diamond {
 
 	public static final char START_LETTER = 'A';
 
-	public static String[] upTo(char letter) {
-		int distance_between_letters = letter - START_LETTER;
+	public static String[] upTo(char stopLetter) {
+		int distance_between_letters = stopLetter - START_LETTER;
 		String[] diamond = new String[distance_between_letters + 1];
 		String dashes = repeat("-", distance_between_letters);
 		diamond[0] = dashes + START_LETTER + dashes;
